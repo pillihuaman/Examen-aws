de89a843acb82b4bb3ea8877c6e1196a9f45eec2
zarmir <pillihuamanhz@gmail.com>
2025-09-02 13:00:10 -0500

    cambi

--- src/main/java/pillihuaman/com/pe/examen/DynamoDBConfig.java
+++ src/main/java/pillihuaman/com/pe/examen/DynamoDBConfig.java
@@ -1,35 +1,34 @@
 package pillihuaman.com.pe.examen;
 
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pillihuaman.com.pe.examen.repository.orden.dao.Orden;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
 
 import java.net.URI;
-
 @Configuration
 public class DynamoDBConfig {
 
     @Value("${aws.region}")
     private String region;
-
-    @Value("${aws.dynamodb.endpoint:}")
-    private String dynamoEndpoint;
+    @Value("${AWS_DYNAMODB_TABLE_ORDEN}")
+    private String ordenTable;
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
-        DynamoDbClientBuilder builder = DynamoDbClient.builder()
-                .region(Region.of(region));
-
-        if (dynamoEndpoint != null && !dynamoEndpoint.isBlank()) {
-            builder.endpointOverride(URI.create(dynamoEndpoint));
-        }
-
-        return builder.build();
+        return DynamoDbClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
     }
 
     @Bean
@@ -37,5 +36,16 @@ public class DynamoDBConfig {
         return DynamoDbEnhancedClient.builder()
                 .dynamoDbClient(dynamoDbClient)
                 .build();
+    }
+
+    @Bean
+    public DynamoDbTable<Orden> ordenTable(DynamoDbEnhancedClient enhancedClient) {
+        return enhancedClient.table(ordenTable, TableSchema.fromBean(Orden.class));
+    }
+
+    @PostConstruct
+    public void testEnv() {
+        System.out.println("🔑 AWS REGION: " + region);
+        System.out.println("🔑 Orden Table: " + ordenTable);
     }
 }
